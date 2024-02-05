5dfe83b45201a351912fa41b1f948f4a029f4a7a
Chaeyeon Seo <ssallyseo@gmail.com>
2024-02-05 23:43:44 +0900

    상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -40,9 +40,12 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    // 상품 등록
-    @PostMapping("/add")
-    public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model) {
+    // 상품 등록 1 - @RequestParam
+    /*@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
         Item item = new Item();
         item.setItemName(itemName);
         item.setPrice(price);
@@ -53,7 +56,43 @@ public class BasicItemController {
         model.addAttribute("item", item);
 
         return "basic/item";
+    }*/
+
+    // 상품 등록 2
+    /**
+     * @ModelAttribute("item") Item item
+     * model.addAttribute("item", item); 자동 추가
+     */
+   /* @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+        return "basic/item";}*/
+
+    // 상품 등록 3 - ModelAttribute 이름 생략
+    /**
+     * @ModelAttribute name 생략 가능
+     * model.addAttribute(item); 자동 추가, 생략 가능
+     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
+    /*@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }*/
+
+    // 상품 등록 4 - addItemV4 - 상품 등록 처리 - ModelAttribute 전체 생략
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
+     */
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
     }
+
+
 
     /**
      * 테스트용 데이터 추가
